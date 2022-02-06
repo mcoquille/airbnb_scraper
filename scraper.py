ddd1ec3aa05c489ff180189bb91e241508e099fa
Marie Coquille <mariecoquille@maries-mbp.lan>
2022-02-06 15:36:40 +0000

    Use txt file instead of pickle

--- scraper.py
+++ scraper.py
@@ -1,12 +1,11 @@
 from lib import scraping, airbnb
-import pickle
 
 # Initiate driver
 driver = scraping.get_driver()
 
 # Get the list of room IDs we want to scrape
-with open('room_ids.pickle', 'rb') as handle:
-    room_ids = pickle.load(handle)
+with open('room_ids.txt', 'r') as f:
+    room_ids = [line.strip() for line in f]
 
 # Initiate the list of room IDs we won't be able to scrape
 failed_room_ids = []
@@ -15,6 +14,7 @@ for room_id in room_ids:
     room = airbnb.scrape_room(driver, room_id)
     if room:
         # Add data in db
+        print(f"Successfully processed {room_id}")
         pass
     else:
         failed_room_ids.append(room_id)
